74592d3fb081fde7133d3f440e100de3742fa702
Ronald Castro_pavilion <ronaldivan.1210868@correo.uis.edu.co>
2023-02-02 00:19:59 -0500

    Orientar nuestra API al dominio con MapStruct

---
+++ demo/src/main/java/com/platzi/demo/persistence/mapper/CategoryMapper.java
@@ -0,0 +1,24 @@
+package com.platzi.demo.persistence.mapper;
+
+
+import com.platzi.demo.domain.Category;
+import com.platzi.demo.persistence.entity.Categoria;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+
+    @Mappings({
+            @Mapping(source="idCategoria",target="categoryId"),
+            @Mapping(source="categoria",target="category"),
+            @Mapping(source="estado",target="categoryId")
+    })
+    Category toCategory(Categoria categoria);
+
+    @InheritInverseConfiguration
+    @Mapping(target="productos",ignore=true)
+    Categoria toCategoria(Category category);
+}
