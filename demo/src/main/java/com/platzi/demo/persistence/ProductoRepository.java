d29901cd76008d0e04edf01ee5d807484ab5d215
Ronald Castro_pavilion <ronaldivan.1210868@correo.uis.edu.co>
2023-02-01 23:28:45 -0500

    anotación @Repository en la clase y terminar querys

--- demo/src/main/java/com/platzi/demo/persistence/ProductoRepository.java
+++ demo/src/main/java/com/platzi/demo/persistence/ProductoRepository.java
@@ -2,10 +2,13 @@ package com.platzi.demo.persistence;
 
 import com.platzi.demo.persistence.crud.ProductoCrudRepository;
 import com.platzi.demo.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+
+@Repository
 public class ProductoRepository {
 
     private ProductoCrudRepository productoCrudRepository;
@@ -20,5 +23,20 @@ public class ProductoRepository {
     public Optional<List<Producto>> getEscasos(int cantidad){
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad,true);
     }
+
+    public Optional<Producto>getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
+    }
+
 
 }
